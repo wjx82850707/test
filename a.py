9e28704ae8a67cc95157f14a4bc9b6e803f22aa6
windwei(魏佳新) <windwei@tencent.com>
2018-04-17 20:31:23 +0800

    first commit

---
+++ a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+def test(a):
+    if(a!="a"):
+      raise Exception("dddddddddd")
+    return
+
+def test2():
+     a = "1|2".split("|")
+     b = "1|3".split("|")
+     c = "4|5".split("|")
+     al=[]
+     al.extend(b)
+     al.extend(c)
+     al= list(set(al))
+     for ip in al :
+         print(ip)
+
+def aaa(serverName):
+    if "tt"==serverName:
+        return "kk"
+    else:
+        return "aaaaa"
+
+if __name__ == '__main__':
+   print(aaa("tt"))
